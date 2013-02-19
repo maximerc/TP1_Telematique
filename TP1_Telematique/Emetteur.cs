758a23fe6bc63f39fd4923b6a4113f425574d6ca
Antoine4 <tony.c420@hotmail.com>
2013-02-19 14:28:51 -0500

    shit dans emetteur et trame pas fou ben ben (reverse-merged from commit c740bf6f3302042e2f9054ca9a2e82e68e0c7552)

--- TP1_Telematique/Emetteur.cs
+++ TP1_Telematique/Emetteur.cs
@@ -7,5 +7,13 @@ namespace TP1_Telematique
 {
     class Emetteur
     {
+        public Emetteur(int tailleTampon)
+        {
+            //une trame réseau de « N » octets
+            int N = 5;
+
+            Trame trame = new Trame(N);
+            Trame tamponEnvoie = new Trame(N, tailleTampon);
+        }
     }
 }
