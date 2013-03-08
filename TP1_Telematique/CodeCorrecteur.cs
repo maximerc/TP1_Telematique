4373146f699a3438522ad6558f4d05974615022a
Nospheratus <vincentf26@gmail.com>
2013-03-08 17:18:34 -0500

    CodeCorrecteur init

---
+++ TP1_Telematique/CodeCorrecteur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TP1_Telematique
+{
+    class CodeCorrecteur
+    {
+        public string ajouterCode(byte[] donnees)
+        {
+            string donneesBin = "";
+
+            foreach (byte octet in donnees)
+            {
+                donneesBin += Convert.ToString(octet, 2).PadLeft(8, '0');
+            }
+
+            string bitsCorrecteurs = "";
+
+        }
+    }
+}
