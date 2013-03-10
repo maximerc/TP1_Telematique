2f114165e36fd56d3876f83ecf566aed5b05634c
Keven <keven.fecteau@usherbrooke.ca>
2013-03-10 19:11:07 -0400

    missing file

---
+++ TP1_Telematique/Constantes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP1_Telematique
+{
+    public static class Constantes
+    {
+        public const int TAILLE_DONNEE_TRAME = 420;
+
+        public const byte TYPE_DONNEES = 0;
+        public const byte TYPE_ACK = 1;
+        public const byte TYPE_NAK = 2;
+    }
+}
